114bd6b83cff551bad66263ea302195b8fc57760
imtiyaz786 <iali75637@gmail.com>
2021-08-06 11:37:13 +0530

    week2 work

---
+++ src/week2_work/MinimumPrice.java
@@ -0,0 +1,31 @@
+package week2_work;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MinimumPrice {
+	public static void main(String[] args) {
+		List<Bus> busList = new ArrayList<>();
+		busList.add(new Bus(100, "Abc Travels", 1200.22, 2.3f));
+		busList.add(new Bus(101, "Abc Travels", 1100.22, 2.4f));
+		busList.add(new Bus(190, "Xyz Travels", 1100.22, 2.3f));
+		busList.add(new Bus(200, "Jee Travels", 1000.22, 4.3f));
+		busList.add(new Bus(104, "Java Travels", 900.22, 4.3f));
+		busList.add(new Bus(111, "Xyz Travels", 1200.22, 4.3f));
+		
+		/* busList.stream()
+		.filter(e->e.getRating()>4)
+		.collect(Collectors.toList())
+		.forEach(e->System.out.println(e)); */
+	
+
+
+		System.out.print("min price for the xyz travels : "); 
+		
+		Optional<Bus>  MinXyzTravels=busList.stream().filter(e->e.getProviderName().equals("Xyz Travels")).reduce((c1,c2)->c1.getCost()<c2.getCost()?c1:c2);
+		
+		
+		System.out.print(MinXyzTravels);
+		
+	}
+
+}
